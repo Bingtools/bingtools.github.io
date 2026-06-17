8533d04b99c1a094d6ffc044671f2694851743b1
Bingo_maya_tools <331908200@qq.com>
2026-06-17 18:07:08 +0800

    Update copy paste tool screenshot - tools.js

--- tools.js
+++ tools.js
@@ -102,7 +102,7 @@ cp.show()`,
     screenshots: [
       {
             "title": "暴力帧拷贝界面",
-            "src": "assets/tool-01-copy-paste.png",
+            "src": "assets/tool-copy-paste-new.png",
             "caption": "一键复制/粘贴控制器姿态与动画数据"
       }
 ],
